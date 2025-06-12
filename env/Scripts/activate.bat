d9002cab27c6c03ee261c96f948242c3cac38678
karthikeyen-creations <55886253+karthikeyen-creations@users.noreply.github.com>
2025-06-12 13:36:56 +0530

    u

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\GX171TT\OneDrive - EY\Documents\Notes\2025\June25\pycdes\env"
+set "VIRTUAL_ENV=C:\Users\GX171TT\OneDrive - EY\Documents\Notes\2025\June25\june25-pycodes\env"
 
 if not defined PROMPT set PROMPT=$P$G
 
